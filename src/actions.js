88ba89721824346d4eb670cc78599872081ecdec
Aleksander Grzybowski <kelog@orange.pl>
2017-10-26 20:41:24 +0200

    Abstract away API

--- src/actions.js
+++ src/actions.js
@@ -1,30 +1,17 @@
-const DATABASE = [
-    {
-        id: 1,
-        title: 'All the things you are',
-        backingTrackUrl: 'https://www.youtube.com/watch?v=iyHDHYrdWPs',
-        practiceLog: ['2017-10-23', '2017-10-25']
-    },
-    {
-        id: 2,
-        title: 'Donna Lee',
-        backingTrackUrl: 'https://www.youtube.com/watch?v=fVOTUXfqhhE',
-        practiceLog: ['2017-10-24', '2017-10-25']
-    }
-];
+import * as backend from './backend';
 
 export function loadSongs() {
     return dispatch => {
         dispatch({type: 'REQUEST_START'});
 
-        setTimeout(() => {
+        backend.listSongs().then(songs => {
             dispatch({type: 'REQUEST_FINISH'});
 
             dispatch({
                 type: 'SONGS_LOADED',
-                songs: DATABASE
+                songs: songs
             });
-        }, 1000);
+        })
     }
 }
 
@@ -32,16 +19,12 @@ export function addSong(title, url) {
     return dispatch => {
         dispatch({type: 'REQUEST_START'});
 
-        setTimeout(() => {
+        backend.addSong(title, url).then(() => {
             dispatch({type: 'REQUEST_FINISH'});
-
-            const nextId = DATABASE[DATABASE.length - 1].id + 1;
-            DATABASE.push({id: nextId, title, backingTrackUrl: url});
 
             dispatch(setSongFormVisible(false));
             dispatch(loadSongs());
-
-        }, 500);
+        });
     }
 }
 
---
+++ src/backend.js
@@ -0,0 +1,25 @@
+const DATABASE = [
+    {
+        id: 1,
+        title: 'All the things you are',
+        backingTrackUrl: 'https://www.youtube.com/watch?v=iyHDHYrdWPs',
+        practiceLog: ['2017-10-23', '2017-10-25']
+    },
+    {
+        id: 2,
+        title: 'Donna Lee',
+        backingTrackUrl: 'https://www.youtube.com/watch?v=fVOTUXfqhhE',
+        practiceLog: ['2017-10-24', '2017-10-25']
+    }
+];
+
+export function listSongs() {
+    return Promise.resolve(DATABASE);
+}
+
+export function addSong(title, url) {
+    const nextId = DATABASE[DATABASE.length - 1].id + 1;
+    DATABASE.push({id: nextId, title, backingTrackUrl: url});
+
+    return Promise.resolve({});
+}
