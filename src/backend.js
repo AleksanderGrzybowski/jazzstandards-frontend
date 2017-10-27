e95b328000eea88b3d62b0f28b0380d09539c247
Aleksander Grzybowski <kelog@orange.pl>
2017-10-27 21:02:28 +0200

    Integrate with backend

--- src/backend.js
+++ src/backend.js
@@ -1,38 +1,13 @@
-import moment from 'moment';
-
-const DATABASE = [
-    {
-        id: 1,
-        title: 'All the things you are',
-        backingTrackUrl: 'https://www.youtube.com/watch?v=iyHDHYrdWPs',
-        practiceLog: ['2017-10-23', '2017-10-25']
-    },
-    {
-        id: 2,
-        title: 'Donna Lee',
-        backingTrackUrl: 'https://www.youtube.com/watch?v=fVOTUXfqhhE',
-        practiceLog: ['2017-10-24', '2017-10-25']
-    }
-];
+import axios from 'axios';
 
 export function listSongs() {
-    return withMockDelay(DATABASE);
+    return axios.get('/api/songs').then(({data}) => data);
 }
 
 export function addSong(title, url) {
-    const nextId = DATABASE[DATABASE.length - 1].id + 1;
-    DATABASE.push({id: nextId, title, backingTrackUrl: url, practiceLog: []});
-
-    return withMockDelay({});
+    return axios.post('/api/songs', {title, backingTrackUrl: url});
 }
+
 export function addPractice(songId) {
-    DATABASE.find(song => song.id === songId).practiceLog.push(moment().format('YYYY-MM-DD'));
-
-    return withMockDelay({});
-}
-
-function withMockDelay(resolvedValue) {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(resolvedValue), 500);
-    });
+    return axios.put(`/api/songs/${songId}/practice`);
 }
